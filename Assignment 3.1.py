1042b5e5a836705e6e55c13498d45772bdf498f7
Michae1EP <mikh.shutenko@gmail.com>
2020-03-08 16:36:39 +0100

    Initial commit

---
+++ Assignment 3.1.py
@@ -0,0 +1,20 @@
+score = input("Enter Score: ")
+try:
+    s = float(score)
+except:
+    y
+
+if s >= 0.9 :
+    print('A')
+elif >= 0.8 :
+    print('B')
+elif >= 0.7 :
+    print('C')
+elif >= 0.6 :
+    print('D')
+elif < 0.6 :
+    print('F')
+except:
+
+
+    quit()
